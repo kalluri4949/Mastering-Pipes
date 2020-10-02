294bb005179d3b5bf3a6093dfa227beb719f6f91
rakesh <kallurirakesh15@gmail.com>
2020-10-01 20:22:31 -0400

    transform text with pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string;
+  date: string;
+  amount: number;
+  decimal: number;
+  miles: number;
+  car = {
+    make: 'mini',
+    color: 'orange',
+    model: 'sport',
+  };
+  onNameChange(value: string): void {
+    this.name = value;
+  }
+  onDateChange(date: string): void {
+    this.date = date;
+  }
+  onAmountChange(value: string): void {
+    this.amount = parseFloat(value);
+  }
+  onDecimalChange(value: string): void {
+    this.decimal = parseFloat(value);
+  }
+  onMilesChange(value: string): void {
+    this.miles = parseFloat(value);
+  }
 }
